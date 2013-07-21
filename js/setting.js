b7a10c163a350637a3447d463814234a237e43ec
m-etou <m-etou@chklab.com>
2013-07-21 18:34:11 +0900

    佐伯市のデータを表示するように変更

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'saiki_budget_2011';
-OpenSpending.year = '2012';
+OpenSpending.identifier = '2011saiki_budget';
+OpenSpending.year = '2011';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
